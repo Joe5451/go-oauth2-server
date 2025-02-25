6382cb650f0f7aa4f87be81daa7fe4b39fa069bc
Joe5451 <skybluejoe123@gmail.com>
2025-02-25 22:45:59 +0800

    chore: modify .env.example and set the environment variables to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,8 +7,22 @@ import (
 )
 
 type Config struct {
+	DBHost     string `mapstructure:"DB_HOST"`
+	DBPort     string `mapstructure:"DB_PORT"`
+	DBUser     string `mapstructure:"DB_USER"`
+	DBPassword string `mapstructure:"DB_PASSWORD"`
+	DBName     string `mapstructure:"DB_NAME"`
+
 	GoogleOauth2ClientID     string `mapstructure:"GOOGLE_OAUTH2_CLIENT_ID"`
 	GoogleOauth2ClientSecret string `mapstructure:"GOOGLE_OAUTH2_CLIENT_SECRET"`
+
+	FacebookOauth2ClientID     string `mapstructure:"FACEBOOK_OAUTH2_CLIENT_ID"`
+	FacebookOauth2ClientSecret string `mapstructure:"FACEBOOK_OAUTH2_CLIENT_SECRET"`
+
+	TwitchOauth2ClientID     string `mapstructure:"TWITCH_OAUTH2_CLIENT_ID"`
+	TwitchOauth2ClientSecret string `mapstructure:"TWITCH_OAUTH2_CLIENT_SECRET"`
+
+	CSRFSecure bool `mapstructure:"CSRF_SECURE"`
 }
 
 var AppConfig Config
