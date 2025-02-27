8923ca28ef88b7ed972e2ee734881fb1def67108
Joe5451 <skybluejoe123@gmail.com>
2025-02-27 23:20:34 +0800

    refactor: remove obsolete files of user domain after integration

--- internal/domains/userrepository.go
+++
@@ -1,9 +0,0 @@
-package domains
-
-type UserRepository interface {
-	Create(user User) User
-	GetSocialAccountByProviderUserId(provider, providerUserId string) *SocialAccount
-	CreateSocialAccount(account SocialAccount) SocialAccount
-	GetUserById(id int64) User
-	UpdateUserById(id int64, user User)
-}
