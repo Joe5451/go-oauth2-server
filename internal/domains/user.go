74f31130e991c8adab8f68ef1236c349fc4020f5
Joe5451 <skybluejoe123@gmail.com>
2025-02-27 23:18:52 +0800

    feat: add user domain interface

--- internal/domains/user.go
+++ internal/domains/user.go
@@ -1,6 +1,10 @@
 package domains
 
-import "time"
+import (
+	"time"
+
+	"github.com/Joe5451/go-oauth2-server/internal/socialproviders"
+)
 
 type User struct {
 	ID          int64     `json:"id" gorm:"primaryKey"`
@@ -13,4 +17,32 @@ type User struct {
 	Birthday    time.Time `json:"birthday"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type SocialAccount struct {
+	ID             int64     `json:"id"`
+	UserID         int64     `json:"user_id"`
+	Provider       string    `json:"provider"`
+	ProviderUserID string    `json:"provider_user_id"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+	User           User      `gorm:"references:ID"`
+}
+
+type UserUsecase interface {
+	Register(user User) error
+	LoginWithEmail(email, password string) (User, error)
+	GenerateSocialProviderAuthUrl(provider socialproviders.SocialProvider, state, redirectUri string) (string, error)
+	LoginWithSocialAccount(provider socialproviders.SocialProvider, authorizationCode, redirectUri string) (User, error)
+	GetUserByID(userID int64) User
+	UpdateUserByID(userID int64, user User)
+}
+
+type UserRepository interface {
+	Create(user User) (User, error)
+	GetUserByEmail(email string) (User, error)
+	GetSocialAccountByProviderUserID(provider, providerUserID string) *SocialAccount
+	CreateSocialAccount(account SocialAccount) SocialAccount
+	GetUserByID(id int64) (User, error)
+	UpdateUserByID(id int64, user User)
 }
