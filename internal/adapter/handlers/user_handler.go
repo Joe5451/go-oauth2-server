dd77824bca2bad98612136427e413ded3125dcdc
Joe5451 <skybluejoe123@gmail.com>
2025-03-07 22:59:00 +0800

    feat: add Register to user handler

--- internal/adapter/handlers/user_handler.go
+++ internal/adapter/handlers/user_handler.go
@@ -1,7 +1,10 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/Joe5451/go-oauth2-server/internal/application/ports/in"
+	"github.com/gin-gonic/gin"
 )
 
 type UserHandler struct {
@@ -12,4 +15,36 @@ func NewUserHandler(usecase in.UserUsecase) *UserHandler {
 	return &UserHandler{
 		usecase: usecase,
 	}
+}
+
+func (h *UserHandler) Register(c *gin.Context) {
+	json := struct {
+		Email    string `json:"email" binding:"required"`
+		Password string `json:"password" binding:"required"`
+		Username string `json:"name" binding:"required"`
+	}{}
+
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "請求參數錯誤，請檢查輸入",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	err := h.usecase.Register(in.RegisterUserRequest{
+		Email:    json.Email,
+		Password: json.Password,
+		Username: json.Username,
+	})
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "發生錯誤",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	c.Status(http.StatusNoContent)
 }
