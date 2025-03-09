f4ecdfb7c1d55c48827b32f8a66f2d67e2173738
Joe5451 <skybluejoe123@gmail.com>
2025-03-09 22:24:56 +0800

    feat: modify social_account domain model

--- internal/domain/social_account.go
+++ internal/domain/social_account.go
@@ -1,16 +1,17 @@
 package domain
 
 import (
-	"database/sql"
 	"time"
 )
 
 type SocialAccount struct {
-	ID             int64         `json:"id"`
-	UserID         sql.NullInt64 `json:"user_id"`
-	Provider       string        `json:"provider"`
-	ProviderUserID string        `json:"provider_user_id"`
-	CreatedAt      time.Time     `json:"created_at"`
-	UpdatedAt      time.Time     `json:"updated_at"`
-	User           User          `gorm:"references:ID"`
+	ID             int64     `json:"id"`
+	UserID         *int64    `json:"user_id"`
+	Provider       string    `json:"provider"`
+	ProviderUserID string    `json:"provider_user_id"`
+	Email          *string   `json:"email"`
+	Name           *string   `json:"name"`
+	Avatar         *string   `json:"avatar"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 }
