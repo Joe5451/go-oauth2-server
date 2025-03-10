b2586984cc2008bd21a4ebe934fe1ec655e06a32
Joe5451 <skybluejoe123@gmail.com>
2025-03-10 22:39:14 +0800

    feat: add SocialAccounts property to user domain model

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -5,11 +5,12 @@ import (
 )
 
 type User struct {
-	ID        int64     `json:"id"`
-	Email     string    `json:"email"`
-	Password  string    `json:"-"`
-	Name      string    `json:"name"`
-	Avatar    *string   `json:"avatar"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID             int64   `json:"id"`
+	Email          string  `json:"email"`
+	Password       string  `json:"-"`
+	Name           string  `json:"name"`
+	Avatar         *string `json:"avatar"`
+	SocialAccounts []SocialAccount
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 }
