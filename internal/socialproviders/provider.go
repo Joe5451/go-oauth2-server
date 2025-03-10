294efdc4d93cbc4712c13984a269a14a5f6a7ac7
Joe5451 <skybluejoe123@gmail.com>
2025-03-10 22:40:07 +0800

    feat: add NewSocialProvider to social providers

--- internal/socialproviders/provider.go
+++ internal/socialproviders/provider.go
@@ -16,3 +16,14 @@ type SocialProviderUser struct {
 	Username       string
 	Avatar         string
 }
+
+func NewSocialProvider(provider string) (SocialProvider, error) {
+	switch provider {
+	case "google":
+		return NewGoogleProvider(), nil
+	case "facebook":
+		return NewFacebookProvider(), nil
+	default:
+		return nil, fmt.Errorf("unsupported provider: %s", provider)
+	}
+}
