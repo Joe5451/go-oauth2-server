c465deb14a263d390aa4755feb8716f7e921e220
Joe5451 <skybluejoe123@gmail.com>
2025-03-11 22:37:11 +0800

    feat: set up a cookie-based session in router temporarily

--- internal/http/router.go
+++ internal/http/router.go
@@ -2,12 +2,18 @@ package http
 
 import (
 	"github.com/Joe5451/go-oauth2-server/internal/adapter/handlers"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 
 	"github.com/gin-gonic/gin"
 )
 
 func NewRouter(userHandler *handlers.UserHandler) *gin.Engine {
 	router := gin.Default()
+
+	// Set up a cookie-based session temporarily
+	store := cookie.NewStore([]byte("secret"))
+	router.Use(sessions.Sessions("usersession", store))
 
 	// setup csrf middleware
 	router.Use(middlewares.CSRF())
